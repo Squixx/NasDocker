8103471dcb16398ed7cdecfd81574c0d8c93b99f
Squixx <mavandermeij@gmail.com>
2018-02-24 16:27:49 +0100

    fix mariadb after update

--- Docker_container_run_commands/mysql.sh
+++ Docker_container_run_commands/mysql.sh
@@ -1,8 +1,8 @@
 docker run -d  \
---restart=unless-stopped \
+           --restart=unless-stopped \
            --name mariadb \
            -e ALLOW_EMPTY_PASSWORD=yes \
-           -e MARIADB_ROOT_PASSWORD=** \
+           -e MARIADB_ROOT_PASSWORD=root \
            -p 3306:3306 \
-           --mount 'type=volume,src=mysqldata,volume-driver=local,dst=/var/lib/mysql,volume-opt=type=nfs,volume-opt=device=:/mnt/workdisk/docker/mysqldata,"volume-opt=o=addr=192.168.1.98,vers=4,soft,timeo=180,rsize=1048576,wsize=1048576,retrans=2"' \
+           --mount 'type=volume,src=mariadb,volume-driver=local,dst=/bitnami,volume-opt=type=nfs,volume-opt=device=:/mnt/workdisk/docker/mysqlconfig,"volume-opt=o=addr=192.168.1.98,vers=4,soft,timeo=180,rsize=1048576,wsize=1048576,retrans=2"' \
            bitnami/mariadb:latest
